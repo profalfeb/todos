30e9d619116640b25b427ebb137aa5d3e0956f38
profalfeb <profalfeb@gmail.com>
2023-02-09 01:33:17 -0400

    Agregando más archivos al proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoItem } from './components/TodoItem';
+import { TodoList } from './components/TodoList';
+import { CreateTodoButton } from './components/CreateTodoButton';
+
+const todos = [
+  {text: "Cortar la cebolla"},
+  {text: "Tomar el curso introductorio de React"},
+  {text: "Llorar con la cebolla"}
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter />
+      <TodoSearch />
+        <input placeholder='Cebolla'/>
+      
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem />
+        ))}
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton />
+      <button>+</button>
+    </>
   );
 }
 
---
+++ src/components/TodoSearch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoSearch = () => {
+  return (
+    <div>TodoSearch</div>
+  )
+}
+
+export { TodoSearch }
